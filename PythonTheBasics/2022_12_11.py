6fa3f968091087d972f21c45cdfad92a3eb7edb3
Elvis Munteanu <entirelymagic@gmail.com>
2022-12-11 08:33:13 +0200

    Initial commit

---
+++ PythonTheBasics/2022_12_11.py
@@ -0,0 +1,8 @@
+from math import pi
+
+
+def calculeaza_aria_unui_cerc(raza):
+    return pi * raza ** 2
+
+C1 = calculeaza_aria_unui_cerc(5)
+print(C1)
---
+++ PythonTheBasics/exercitii/MyFirstProgram.py
@@ -0,0 +1,1 @@
+print("Salut grupa PythonRemoteRo41!")
